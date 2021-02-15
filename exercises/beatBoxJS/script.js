91d0f3affd202feeede29873bea9b610f4e46fb4
Atul Rauthan <atulrauthan7@gmail.com>
2021-02-15 14:47:51 +0530

    final project

--- exercises/beatBoxJS/script.js
+++ exercises/beatBoxJS/script.js
@@ -18,30 +18,39 @@
 let beats = {
     "65": {
         beat: new Beat("./assets/Piano Chord 331.mp3"),
+        button: new Button("#00fffe", 65)
     },
     "83": {
         beat: new Beat("./assets/Piano Chord 209.mp3"),
+        button: new Button("#00fffe", 83)
     },
     "68": {
         beat: new Beat("./assets/Piano Chord 208.mp3"),
+        button: new Button("#00fffe", 68)
     },
     "70": {
         beat: new Beat("./assets/Drum Sticks Hit 3.mp3"),
+        button: new Button("#FF00FF", 70)
     },
     "71": {
         beat: new Beat("./assets/Drum Snare Roll.mp3"),
+        button: new Button("#FF00FF", 71)
     },
     "72": {
         beat: new Beat("./assets/PREL Musical 57.mp3"),
+        button: new Button("#FF00FF", 72)
     },
     "74": {
         beat: new Beat("./assets/Cymbal Suspended 2.mp3"),
+        button: new Button("#FF00FF", 74)
     },
     "75": {
         beat: new Beat("./assets/Musical Compos 33.mp3"),
+        button: new Button("#FFFFFF", 75)
     },
     "76": {
         beat: new Beat("./assets/Musical Orches 4.mp3"),
+        button: new Button("#FFFFFF", 76)
     }
 }
 
@@ -52,6 +61,7 @@ let beats = {
 triggerBeat = (event) => {
     if(event.keyCode in beats) {
         beats[event.keyCode].beat.play()
+        beats[event.keyCode].button.select()
     }   
 }
 
--- exercises/beatBoxJS/utils.js
+++ exercises/beatBoxJS/utils.js
@@ -21,23 +21,39 @@ class Beat {
  */
 class Button {
     constructor(color, keyCode){
+        this.color = color
+        this.keyCode = keyCode
+        this.element = document.getElementById(keyCode)
+        this.setButtonColorInHTML()
+        this.setAtTransitionEndListener()
+    }
+
+    setAtTransitionEndListener = () => {
+        this.element.addEventListener('transitionend', () => {
+            this.deselect()
+        })
     }
 
     /**
      * Set the button color based on color specified
      */
     setButtonColorInHTML = () => {
+        this.element.style.borderColor = this.color
     }
 
     /**
      * Select function to set the background color and boxShadow
      */
     select = () => {
+        this.element.style.backgroundColor = this.color
+        this.element.style.boxShadow = `0px 0px 17px 0px ${this.color}`
     }
 
     /**
      * Deselect function to reset background color and boxShadow
      */
     deselect = () => {
+        this.element.style.backgroundColor = "transparent"
+        this.element.style.boxShadow = "none"
     }
 }
